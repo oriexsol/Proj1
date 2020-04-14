a84f8d91e7da25e6126738dde2faa36c34fdb251
ori <oriexsol123@gmail.com>
2020-04-14 18:33:27 +0300

    kube start

---
+++ kube-start.sh
@@ -0,0 +1,8 @@
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/2140ac876ef134e0ed5af15c65e414cf26827915/Documentation/kube-flannel.yml
+
